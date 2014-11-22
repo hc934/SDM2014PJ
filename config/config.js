9e57e6b3c8a9afde9c15349cf05621fdc5d73939
pilagod <r03725029@ntu.edu.tw>
2014-11-22 21:50:40 +0800

    Modified config/config.js

--- config/config.js
+++ config/config.js
@@ -1,3 +1,8 @@
 var config = {
-  'This_is': 'a_config_file'
+  'This_is': 'a_config_file',
+  'db_host': 'sdm2.im.ntu.edu.tw',
+  'db_port': '3306',
+  'db_username': '2014Group3',
+  'db_password': '6jQkcT79',
+  'db_name': '2014Group3'
 };
