05b50ae3417e0a93af812835c1060af5b71987b7
Vison <kk801030@gmail.com>
2014-11-25 00:40:16 +0800

    forum articles: relate to json

--- public/js/forum.js
+++ public/js/forum.js
@@ -1,6 +1,15 @@
 app = angular.module('forum',[]);
 
-app.controller('ArticleController',['$scope',function($scope){
-	$scope.article = [];
-	
+app.controller('ArticleController',['$scope','$http',function($scope,$http){
+	$scope.articles = [];
+	$http({
+        method: 'GET',   
+        url: '/json/article.json'
+    }).success(function(data, status, headers, config) {   
+        $scope.articles = data;
+        
+    }).error(function(data, status, headers, config) {   
+           
+    });  
 }]);
+
