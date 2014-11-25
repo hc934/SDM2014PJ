8a56160a6a94f5fb2719ecab7c741aa282d69ea3
Vison <kk801030@gmail.com>
2014-11-26 03:20:39 +0800

    could submit a comment

--- public/js/forum.js
+++ public/js/forum.js
@@ -33,6 +33,21 @@ app.controller('PostController',['$scope','$http',function($scope,$http){
 
 app.controller('ArticleController',['$scope', '$http', function($scope, $http){
   $scope.article;
+  $scope.comment;
+  $scope.submitComment = function(){
+    var data = {
+      content: $scope.comment,
+    }
+    $http.post('/api/'+article_id+'/comment',data).
+      success(function(data, status, headers, config) {   
+        alert('新增成功！');
+        window.location.href = window.location.href;
+      }).
+      error(function(data, status, headers, config) {   
+        console.log(data);
+      }); 
+  };
+
   var temp_array = location.href.split('/');
   var article_id = temp_array[temp_array.length-1];
   $http.get('/api/article/'+article_id).
