bdfc9cb79a0b4079fe77dd7a5dbdf88ae448fa41
Vison <kk801030@gmail.com>
2014-11-26 02:20:44 +0800

    check validation of input in login page

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,12 @@ app = angular.module('login', []);
 
 app.controller('LoginController', ['$scope', function($scope){
 		$scope.user;
-    
+    // check the inputs are valid
+    $scope.check = function(valid){
+      if (valid) {
+        $scope.login();
+      }
+    }
 		$scope.login = function(){
 			// check login information
       console.log($scope.user);
