3d93cb02122048ea89cdb0a9b9152dfe71bd31af
2014Group3 <yknip1207@hotmail.com>
2014-12-13 22:39:40 +0800

    Job controller data parsing complete

--- public/js/job.js
+++ public/js/job.js
@@ -7,8 +7,12 @@ angular.module('job', ['lang'])
             job_type: "Programmer"
         },
         {
-            corporation: "Yahoo",
+            corporation: "Yahoo!",
             job_type: "UI/UX Designer"
+        },
+         {
+            corporation: "ROC army",
+            job_type: "Commander"
         }
     ];
 }])
@@ -27,4 +31,40 @@ angular.module('job', ['lang'])
         language_requirement: "Japanese/Chinese",
         other_requirement: "Handsome, Billionaire and Handsome, Billionaire"
     };
+}])
+
+.controller('NewJobController', ['$scope','$http',function($scope,$http){
+
+
+    $scope.sendMessage = function(message) {
+        var data = {
+            corporation: ($scope.job_model.corporation == undefined)?"":$scope.job_model.corporation,
+            job_type: ($scope.job_model.job_type == undefined)?"":$scope.job_model.job_type,
+            location: ($scope.job_model.location == undefined)?"":$scope.job_model.location,
+            work_type: ($scope.job_model.work_type == undefined)?"":$scope.job_model.work_type,
+            payment: ($scope.job_model.payment == undefined)?"":$scope.job_model.payment,
+            chracters: ($scope.job_model.chracters == undefined)?"":$scope.job_model.chracters,
+            work_experience: ($scope.job_model.work_experience == undefined)?"":$scope.job_model.work_experience,
+            education: ($scope.job_model.payment == undefined)?"":$scope.job_model.education,
+            major_in: ($scope.job_model.payment == undefined)?"":$scope.job_model.major_in,
+            language_requirement: ($scope.job_model.payment == undefined)?"":$scope.job_model.language_requirement,
+            other_requirement: ($scope.job_model.payment == undefined)?"":$scope.job_model.other_requirement
+        };
+
+        console.log(data);
+
+            
+    };
+
+
+    // $scope.placeholder = function() {
+    //     return function(input){
+    //         if(!(input == undefined || input == null)){
+    //           return input;
+    //         } else {
+    //           return "placeholder";
+    //         }
+    //     }
+    // };
+
 }]);
