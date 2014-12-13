8faf5e06ea0fea72aa8e1d6d0f34a591ebc89e92
Vison <kk801030@gmail.com>
2014-12-13 17:27:11 +0800

    Profile: 以block分開進行update, clear

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,23 +2,49 @@ angular.module('profile', ['lang'])
   
 
 .controller('ProfileController', ['$scope', '$http', function($scope, $http){
-    $scope.info;
-    
 
     $scope.session = sessionStorage.getItem('id');
     data = {
       id: $scope.session
     }
     $scope.user;
-    $http.post('/api/profile', data).
-    success(function(data, status, headers, config) {
-      $scope.user = data;
-      // console.log(data);
-    }).
-    error(function(data, status, headers, config) {
-      // console.log(data);
-    });
-  }])
+    $scope.edit_user;
+    $scope.init = function() {
+      $http.post('/api/profile', data).
+            success(function(data, status, headers, config) {
+              $scope.user = data;
+              $scope.edit_user = jQuery.extend({}, $scope.user); // copy of user 
+              // console.log(data);
+            }).
+            error(function(data, status, headers, config) {
+              // console.log(data);
+            });
+     };
+     $scope.update = function(block) {
+      switch(block) {
+        case 'contact':
+          $scope.user.name = $scope.edit_user.name;
+          $scope.user.email = $scope.edit_user.email;
+          $scope.user.mobile_phone = $scope.edit_user.mobile_phone;
+        break;
+        case 'experience':
+          $scope.experience = jQuery.extend({}, $scope.experience);
+        break;
+        case 'education':
+          $scope.education = jQuery.extend({}, $scope.education);
+        break;
+      }
+
+      if ($http.put('/api/profile', $scope.user)) {
+        alert("更新成功")
+        window.location.href = '/profile';
+      }
+    };
+
+    $scope.clear = function() {
+      $scope.edit_user = jQuery.extend({}, $scope.user); // copy of user 
+    };
+}])
 
 .controller('ProfileEditController', ['$scope', '$http', function($scope, $http){
     $scope.session = sessionStorage.getItem('id');
@@ -30,6 +56,7 @@ angular.module('profile', ['lang'])
     $http.post('/api/profile', data).
     success(function(data, status, headers, config) {
       $scope.user = data;
+
     }).
     error(function(data, status, headers, config) {
       // console.log(data);
