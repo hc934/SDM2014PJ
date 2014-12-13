3f307c4ec0746844ce1ba8625bb1c898e3e7b95e
Vison <kk801030@gmail.com>
2014-12-13 17:58:38 +0800

    Profile:block edit done

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,11 +2,16 @@ angular.module('profile', ['lang'])
   
 
 .controller('ProfileController', ['$scope', '$http', function($scope, $http){
-
+    $scope.edit = {
+      info : false,
+      exp : false,
+      edu : false
+    }
     $scope.session = sessionStorage.getItem('id');
     data = {
       id: $scope.session
     }
+
     $scope.user;
     $scope.edit_user;
     $scope.init = function() {
@@ -22,22 +27,31 @@ angular.module('profile', ['lang'])
      };
      $scope.update = function(block) {
       switch(block) {
-        case 'contact':
+        case 'info':
           $scope.user.name = $scope.edit_user.name;
           $scope.user.email = $scope.edit_user.email;
           $scope.user.mobile_phone = $scope.edit_user.mobile_phone;
         break;
-        case 'experience':
+        case 'exp':
           $scope.experience = jQuery.extend({}, $scope.experience);
         break;
-        case 'education':
+        case 'edu':
           $scope.education = jQuery.extend({}, $scope.education);
         break;
       }
 
       if ($http.put('/api/profile', $scope.user)) {
-        alert("更新成功")
-        window.location.href = '/profile';
+        switch(block) {
+          case 'info':
+            $scope.edit.info = false;
+          break;
+          case 'exp':
+            $scope.edit.exp = false;
+          break;
+          case 'edu':
+            $scope.edit.edu = false;
+          break;
+        }
       }
     };
 
